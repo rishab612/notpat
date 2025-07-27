0101f2ee53152b1fcdf66adf5b29caac73402011
rishab612 <116106147+rishab612@users.noreply.github.com>
2025-07-27 15:08:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 const express = require('express');
-const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const wss = new WebSocket.Server({ noServer: true });
 
